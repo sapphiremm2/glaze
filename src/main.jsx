9128be6e375c93b8bdf4e0f1270282db5369659f
Sapphire <sapphiresgfx@gmail.com>
2026-03-03 17:16:15 -0600

    revert to 3cb0926

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,3 @@
-window.onerror = (msg, url, line) => {
-  document.body.innerHTML = `<div style="color:white;padding:20px;">Error: ${msg}<br>Line: ${line}</div>`;
-};
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
