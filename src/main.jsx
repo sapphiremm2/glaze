866b3fec359e5ae60eb93c0fff531d9f31b7d73c
Sapphire <sapphiresgfx@gmail.com>
2026-03-03 12:22:30 -0600

    google auth fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import AuthCallback from './AuthCallback'
 import './index.css'
-import AuthCallback from './AuthCallback';
-
-// Add this route
-<Route path="/auth/callback" element={<AuthCallback />} />
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/auth/callback" element={<AuthCallback />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
