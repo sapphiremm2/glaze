006236f003980dfafb67fb9bc7a8b14114ddb2a7
Sapphire <sapphiresgfx@gmail.com>
2026-03-03 16:55:10 -0600

    attempting to bring back mobile ios support

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,7 @@
+window.onerror = (msg, url, line) => {
+  document.body.innerHTML = `<div style="color:white;padding:20px;">Error: ${msg}<br>Line: ${line}</div>`;
+};
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
